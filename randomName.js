d95c8b96ea7f081441ea3c5ba1aecd2d8030f147
AADITHYA V ANAND <aadithyadreams@gmail.com>
2024-04-18 15:06:52 +0530

    Create randomName.js
    random name generator using npm module called "sillyname"

---
+++ randomName.js
@@ -0,0 +1,6 @@
+// var generateName = require('sillyname'); using sillyname npm module
+
+import generateName from "sillyname";
+var sillyName = generateName();
+
+console.log(`My name is ${sillyName}`);
